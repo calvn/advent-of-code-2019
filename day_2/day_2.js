a8ac01827526d8d5258e22aa1111016c698a3d1a
Calvin Leung Huang <cleung2010@gmail.com>
2019-12-02 22:54:49 -0800

    day 2

---
+++ day_2/day_2.js
@@ -0,0 +1,62 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+function runProgram(noun, verb) {
+    // Read input
+    // let data: string = '1,0,0,0,99'
+    var data = fs.readFileSync('input/day_2.txt').toString();
+    var dataSlice = data.split(',').map(function (x) { return +x; });
+    // Perform "reset"
+    dataSlice[1] = noun;
+    dataSlice[2] = verb;
+    for (var i = 0; i < dataSlice.length; i = i + 4) {
+        var opIdx = i;
+        var op = dataSlice[opIdx];
+        if (op == 99) {
+            console.log('first value:', dataSlice[0]);
+            return dataSlice[0];
+        }
+        var firstIdx = i + 1;
+        var firstPos = dataSlice[firstIdx];
+        var secondIdx = i + 2;
+        var secondPos = dataSlice[secondIdx];
+        var replaceIdx = i + 3;
+        var replacePos = dataSlice[replaceIdx];
+        // console.log(firstPos, secondPos)
+        // console.log(dataSlice[firstPos], dataSlice[secondPos])
+        switch (op) {
+            case 1:
+                dataSlice[replacePos] = (dataSlice[firstPos] + dataSlice[secondPos]);
+                break;
+            case 2:
+                dataSlice[replacePos] = (dataSlice[firstPos] * dataSlice[secondPos]);
+                break;
+            // case 99:
+            //   console.log('first value:', dataSlice[0])
+            //   return
+            default:
+                console.log('invalid operation');
+                break;
+        }
+    }
+}
+function part1() {
+    runProgram(12, 2);
+}
+function part2() {
+    var noun, verb;
+    bruteforce: for (var i = 0; i < 100; i++) {
+        for (var j = 0; j < 100; j++) {
+            var output = runProgram(i, j);
+            if (output == 19690720) {
+                console.log('noun:', i, 'verb:', j);
+                noun = i;
+                verb = j;
+                break bruteforce;
+            }
+        }
+    }
+    console.log('answer:', 100 * noun + verb);
+}
+part1();
+part2();
---
+++ day_2/day_2.ts
@@ -0,0 +1,72 @@
+import * as fs from 'fs'
+
+function runProgram(noun: number, verb: number): number {
+  // Read input
+  // let data: string = '1,0,0,0,99'
+  let data = fs.readFileSync('input/day_2.txt').toString()
+  let dataSlice = data.split(',').map(x => +x)
+
+  // Perform "reset"
+  dataSlice[1] = noun
+  dataSlice[2] = verb
+
+  for (let i = 0; i < dataSlice.length; i = i + 4) {
+    let opIdx = i
+    let op = dataSlice[opIdx] 
+
+    if (op == 99) {
+      console.log('first value:', dataSlice[0])
+      return dataSlice[0]
+    }
+
+    let firstIdx = i+1
+    let firstPos = dataSlice[firstIdx]
+    let secondIdx = i+2
+    let secondPos = dataSlice[secondIdx]
+    let replaceIdx = i+3
+    let replacePos = dataSlice[replaceIdx]
+
+    // console.log(firstPos, secondPos)
+    // console.log(dataSlice[firstPos], dataSlice[secondPos])
+
+    switch (op) {
+      case 1:
+        dataSlice[replacePos] = (dataSlice[firstPos] + dataSlice[secondPos])
+        break
+      case 2:
+        dataSlice[replacePos] = (dataSlice[firstPos] * dataSlice[secondPos])
+        break
+      // case 99:
+      //   console.log('first value:', dataSlice[0])
+      //   return
+      default:
+        console.log('invalid operation')
+        break
+    }
+  }
+}
+
+function part1() {
+  runProgram(12, 2)
+}
+
+function part2() {
+var noun:number, verb:number
+
+bruteforce:
+  for (let i = 0; i < 100; i++) {
+    for (let j = 0; j < 100; j++) {
+      let output = runProgram(i, j)
+      if (output == 19690720) {
+        console.log('noun:', i, 'verb:', j)
+        noun = i
+        verb = j
+        break bruteforce 
+      }
+    }
+  }
+  console.log('answer:', 100 * noun + verb)
+}
+
+part1()
+part2()
